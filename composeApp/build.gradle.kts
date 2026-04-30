668c4ed1c024b8b808a2b50a0cb0dcfd201cb3ec
victordev22 <victor.dev2290@gmail.com>
2026-04-30 12:36:28 +0200

    Fix gradle syntax dots

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -44,14 +44,17 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
-            
+
             implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.content-negotiation)
-            implementation(libs.ktor.serialization-kotlinx-json)
+            // USAR PUNTOS EN LUGAR DE GUIONES:
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+
             implementation(libs.ktor.client.logging)
             implementation(libs.ktor.client.auth)
             implementation(libs.kotlinx.datetime)
         }
+
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
